3270005b6cd81a0a3d06a2bebffbf091831c157d
na81sa <han80440@kwansei.ac.jp>
2025-05-21 16:50:39 +0900

    ex2

---
+++ ex2.py
@@ -0,0 +1,77 @@
+from ultralytics import YOLO
+import cv2
+import torch
+
+model = YOLO("yolo11x-pose.pt")
+
+results = model("ex1.jpg")
+# 1人目のキー・ポイント(x,y)を獲得する．
+# 2人目はresults[0].keypoints.data[1]に格納されている．
+nodes = results[0].keypoints.data[0][:, :2]
+
+# 骨格のリンク
+links = [
+    [5, 7],
+    [6, 8],
+    [7, 9],
+    [8, 10],
+    [11, 13],
+    [12, 14],
+    [13, 15],
+    [14, 16],
+    [5, 11],
+    [6, 12],
+    [5, 6],
+    [11, 12],
+]
+
+# 入力画像
+img = results[0].orig_img
+
+for n1, n2 in links:
+    # 誤認識のリンクを描画しない．
+    if nodes[n1][0] * nodes[n1][1] * nodes[n2][0] * nodes[n2][1] == 0:
+        continue
+    cv2.line(
+        img,
+        # 2つの座標を整数化し，テンソルからリストにする．
+        nodes[n1].to(torch.int).tolist(),
+        nodes[n2].to(torch.int).tolist(),
+        (0, 0, 255),
+        thickness=3,
+    )
+
+
+    cv2.circle(
+        img,
+        center=(nodes[n1].to(torch.int).tolist()),
+        radius=2,
+        color=(0, 255,255),
+        thickness=2,
+    )
+    cv2.circle(
+    img,
+    center=(nodes[n2].to(torch.int).tolist()),
+    radius=2,
+    color=(0, 255,255),
+    thickness=2,
+    )
+
+x = (nodes[6][0] + nodes[5][0] + nodes[12][0] + nodes[11][0]) / 4
+y = (nodes[6][1] + nodes[5][1] + nodes[12][1] + nodes[11][1]) / 4
+
+cv2.circle(
+    img,
+    center=(int(x),int(y)),
+    radius=2,
+    color=(0, 255,255),
+    thickness=2,
+    )
+print(x)
+
+# 画像を画面に表示する．
+cv2.imshow("", img)
+# キー入力があるまで，停止する．
+cv2.waitKey(0)
+# フレームを閉じる．
+cv2.destroyAllWindows()
