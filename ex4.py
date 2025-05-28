30ef8d0c61ddfe79635832b0f124736fcb95ab39
na81sa <han80440@kwansei.ac.jp>
2025-05-28 16:44:33 +0900

    ex4

---
+++ ex4.py
@@ -0,0 +1,41 @@
+# https://ultralytics.com/images/bus.jpg 中の物体を抽出し，その領域を元の画像に描画せよ
+import cv2
+import torch
+from ultralytics import YOLO
+
+model = YOLO("yolov8x.pt")
+
+results = model.predict("ex2.jpg", conf=0.1)
+
+# 入力画像
+img = results[0].orig_img
+
+# 認識した物体領域を取得する．
+boxes = results[0].boxes
+
+max = 0
+for box in boxes:
+    # 物体領域の始点xy座標を得る．
+    xy1 = box.data[0][0:2]
+    # 物体領域の終点xy座標を得る．
+    xy2 = box.data[0][2:4]
+
+    size = (xy2[0] - xy1[0]) * (xy2[1] - xy1[1])
+
+    if size > max:
+        max = size
+        max_xy1 = xy1
+        max_xy2 = xy2
+
+
+cv2.rectangle(
+    img,
+    max_xy1.to(torch.int).tolist(),
+    max_xy2.to(torch.int).tolist(),
+    (0, 0, 255),
+    thickness=3,
+)
+
+cv2.imshow("", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
